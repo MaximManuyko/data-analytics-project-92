a6a62ce464ea20e9d350a6cfd1561eb42ca019dd
MaximManuyko <manuyko.m@gmail.com>
2023-05-08 14:33:14 +0300

    Добавил SQL запрос к третьему заданию

--- queries.sql
+++ queries.sql
@@ -4,12 +4,12 @@
 
 --Подготовьте в файл top_10_total_income.csv отчет с продавцами у которых наибольшая выручка
 SELECT CONCAT_WS(' ', employees.first_name, employees.last_name) AS name, COUNT(*) AS operations, SUM(sales.quantity * products.price) AS income
-FROM sales
-LEFT JOIN employees ON employees.employee_id = sales.sales_person_id
-LEFT JOIN products ON products.product_id = sales.product_id 
-GROUP BY CONCAT_WS(' ', employees.first_name, employees.last_name)
-order by income desc
-limit 10;
+  FROM sales
+  LEFT JOIN employees ON employees.employee_id = sales.sales_person_id
+  LEFT JOIN products ON products.product_id = sales.product_id 
+ GROUP BY CONCAT_WS(' ', employees.first_name, employees.last_name)
+ order by income desc
+ limit 10;
 
 
 
@@ -31,3 +31,38 @@ HAVING SUM(sales.quantity * products.price) <
             LEFT JOIN products ON products.product_id = sales.product_id 
             GROUP BY sales.sales_person_id) AS subquery)
 order by 2;
+
+
+--Третий отчет содержит информацию о выручке по дням недели. Каждая запись содержит имя и фамилию продавца, день недели и суммарную выручку. Отсортируйте данные по порядковому номеру дня недели и name
+
+--name — имя и фамилия продавца
+--weekday — название дня недели на английском языке
+--income — суммарная выручка продавца в определенный день недели, округленная до целого числа
+
+
+SELECT 
+  name,
+  weekday,
+  sum as income
+FROM (
+  SELECT 
+    e.first_name || ' ' || e.last_name AS name,
+    CASE 
+      WHEN extract(isodow from s.sale_date) = 1 THEN 'Monday'
+      WHEN extract(isodow from s.sale_date) = 2 THEN 'Tuesday'
+      WHEN extract(isodow from s.sale_date) = 3 THEN 'Wednesday'
+      WHEN extract(isodow from s.sale_date) = 4 THEN 'Thursday'
+      WHEN extract(isodow from s.sale_date) = 5 THEN 'Friday'
+      WHEN extract(isodow from s.sale_date) = 6 THEN 'Saturday'
+      WHEN extract(isodow from s.sale_date) = 7 THEN 'Sunday'
+    END AS weekday,
+    round(sum(s.quantity * p.price)) AS sum,
+    extract(isodow from s.sale_date) AS number
+  FROM 
+    sales s 
+    JOIN employees e ON s.sales_person_id = e.employee_id 
+    JOIN products p ON s.product_id = p.product_id
+  GROUP BY 
+    e.first_name, e.last_name, weekday, number
+  ORDER BY 4, 1
+) as t
