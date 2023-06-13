ddfcd060a9e9549b931901e0bafd1a821ec830e7
MaximManuyko <manuyko.m@gmail.com>
2023-06-14 02:54:28 +0300

    Наведение красоты

--- queries.sql
+++ queries.sql
@@ -14,23 +14,23 @@ from customers;
 SELECT  CONCAT_WS(' ', employees.first_name, employees.last_name) AS name,
         COUNT(*) AS operations, 
         SUM(sales.quantity * products.price) AS income
-  FROM sales
-  LEFT JOIN employees ON employees.employee_id = sales.sales_person_id
-  LEFT JOIN products ON products.product_id = sales.product_id 
- GROUP BY CONCAT_WS(' ', employees.first_name, employees.last_name)
- order by income desc
- limit 10;
+    FROM sales
+    LEFT JOIN employees ON employees.employee_id = sales.sales_person_id
+    LEFT JOIN products ON products.product_id = sales.product_id 
+    GROUP BY CONCAT_WS(' ', employees.first_name, employees.last_name)
+    order by income desc
+    limit 10;
 
 --Вариант 2
- SELECT  employees.first_name|| ' ' ||employees.last_name AS name,
+    SELECT  employees.first_name|| ' ' ||employees.last_name AS name,
         COUNT(*) AS operations, 
         SUM(sales.quantity * products.price) AS income
-  FROM sales
-  LEFT JOIN employees ON employees.employee_id = sales.sales_person_id
-  LEFT JOIN products ON products.product_id = sales.product_id 
- GROUP BY employees.first_name|| ' ' ||employees.last_name
- order by income desc
- limit 10;
+    FROM sales
+    LEFT JOIN employees ON employees.employee_id = sales.sales_person_id
+    LEFT JOIN products ON products.product_id = sales.product_id 
+    GROUP BY employees.first_name|| ' ' ||employees.last_name
+    order by income desc
+    limit 10;
 ---------------------
 
 
@@ -39,12 +39,12 @@ SELECT  CONCAT_WS(' ', employees.first_name, employees.last_name) AS name,
 --average_income — средняя выручка продавца за сделку с округлением до целого
 
 WITH subquery AS (
-  SELECT first_name || ' ' || last_name AS name,
+    SELECT first_name || ' ' || last_name AS name,
          ROUND(AVG(quantity * price), 0) AS average_income
-  FROM sales
-  LEFT JOIN employees ON employees.employee_id = sales.sales_person_id 
-  LEFT JOIN products ON products.product_id = sales.product_id 
-  GROUP BY first_name || ' ' || last_name
+    FROM sales
+    LEFT JOIN employees ON employees.employee_id = sales.sales_person_id 
+    LEFT JOIN products ON products.product_id = sales.product_id 
+    GROUP BY first_name || ' ' || last_name
 )
 SELECT name, average_income
 FROM subquery
@@ -59,30 +59,30 @@ ORDER BY average_income;
 
 
 SELECT 
-  name,
-  weekday,
-  sum as income
+    name,
+    weekday,
+    sum as income
 FROM (
-  SELECT 
+    SELECT 
     e.first_name || ' ' || e.last_name AS name,
     CASE 
-      WHEN extract(isodow from s.sale_date) = 1 THEN 'Monday'
-      WHEN extract(isodow from s.sale_date) = 2 THEN 'Tuesday'
-      WHEN extract(isodow from s.sale_date) = 3 THEN 'Wednesday'
-      WHEN extract(isodow from s.sale_date) = 4 THEN 'Thursday'
-      WHEN extract(isodow from s.sale_date) = 5 THEN 'Friday'
-      WHEN extract(isodow from s.sale_date) = 6 THEN 'Saturday'
-      WHEN extract(isodow from s.sale_date) = 7 THEN 'Sunday'
+        WHEN extract(isodow from s.sale_date) = 1 THEN 'Monday'
+        WHEN extract(isodow from s.sale_date) = 2 THEN 'Tuesday'
+        WHEN extract(isodow from s.sale_date) = 3 THEN 'Wednesday'
+        WHEN extract(isodow from s.sale_date) = 4 THEN 'Thursday'
+        WHEN extract(isodow from s.sale_date) = 5 THEN 'Friday'
+        WHEN extract(isodow from s.sale_date) = 6 THEN 'Saturday'
+        WHEN extract(isodow from s.sale_date) = 7 THEN 'Sunday'
     END AS weekday,
     round(sum(s.quantity * p.price)) AS sum,
     extract(isodow from s.sale_date) AS number
-  FROM 
+    FROM 
     sales s 
     JOIN employees e ON s.sales_person_id = e.employee_id 
     JOIN products p ON s.product_id = p.product_id
-  GROUP BY 
+    GROUP BY 
     e.first_name, e.last_name, weekday, number
-  ORDER BY 4, 1
+    ORDER BY 4, 1
 ) as t
 ---------------------
 
@@ -96,8 +96,8 @@ SELECT
     COALESCE(COUNT(customers.age), 0) AS count
 FROM 
     (SELECT '16-25' AS age_category
-     UNION SELECT '26-40'
-     UNION SELECT '40+') AS age_categories
+        UNION SELECT '26-40'
+        UNION SELECT '40+') AS age_categories
 LEFT JOIN 
     customers ON 
     CASE 
